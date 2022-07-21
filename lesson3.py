d411939b968695cca447c3106e9cbe0e43160676
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-07-21 10:35:12 +0100

    testing functions

--- lesson3.py
+++ lesson3.py
@@ -8,6 +8,26 @@ f_temp = float(input("Please enter the temp in F:"))
 c_temp = (f_temp - 32) * 5/9
 print(c_temp)
 
+def f_temp(f_temp):
+    c_temp = (f_temp - 32) * 5/9
+    print(c_temp)
+
+
+f100_in_celsius = f_temp(int(100))
+print (f100_in_celsius)
+
+
+def c_to_f(c_temp):
+    f_temp = int(c_temp) * (9/5) + 32
+    print(f_temp)
+
+c0_in_f = c_to_f(int(0))
+
+
+
+
+
+
 
 def get_force(load, distance):
     return load * distance
