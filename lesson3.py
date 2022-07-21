99b32ed7eb11bb94e6ec2159b2ca84acd7ec96ed
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-07-21 10:12:25 +0100

    physics lessons

--- lesson3.py
+++ lesson3.py
@@ -1,7 +1,14 @@
 train_mass = 22680
-train_acceleration = 1000
-train_distance = 5650
+train_acceleration = 10
+train_distance = 100
 bomb_mass = 1
+
+
+f_temp = float(input("Please enter the temp in F:"))
+c_temp = (f_temp - 32) * 5/9
+print(c_temp)
+
 
 def get_force(load, distance):
     return load * distance
+
