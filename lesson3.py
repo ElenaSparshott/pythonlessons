4d260063350d9954a390a50d2818854dccd5eb8b
Elena Sparshott <65815679+ElenaSparshott@users.noreply.github.com>
2022-07-21 10:52:07 +0100

    physics workshop

--- lesson3.py
+++ lesson3.py
@@ -1,8 +1,7 @@
 train_mass = 22680
-train_acceleration = 10
+train_acceleration = 2
 train_distance = 100
 bomb_mass = 1
-
 
 f_temp = float(input("Please enter the temp in F:"))
 c_temp = (f_temp - 32) * 5/9
@@ -23,12 +22,28 @@ def c_to_f(c_temp):
 
 c0_in_f = c_to_f(int(0))
 
+def get_force(mass, acceleration):
+    return mass * acceleration
+
+train_force = get_force(train_mass, train_acceleration)
+print(train_force)
+
+print("The GE train supplies", train_force, "Newtons of force.")
 
 
+c = 3*10**8
+def get_energy(mass, c):
+    return mass * (c**2)
 
+bomb_energy = get_energy(bomb_mass, c)
+print(bomb_energy)
 
+print("A 1KG bomb supplies", bomb_energy, "joules")
 
+def get_work(mass, acceralation, distance):
+    return get_force(mass, acceralation) * distance
 
-def get_force(load, distance):
-    return load * distance
+train_work = get_work(train_mass, train_acceleration, train_distance)
+print(train_work)  
 
+print("The GE train does", train_work, "joules of work over", train_distance, "meters")
